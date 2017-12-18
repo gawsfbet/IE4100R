717452801bd1eb49efbd40ddb6dbbdfb0416e4a9
DESKTOP-H13VE6F\Kevin <dragoN414@gmail.com>
2017-12-19 00:39:07 +0800

    implemented csv file reader

--- src/ie4100r/Main.java
+++ src/ie4100r/Main.java
@@ -5,16 +5,68 @@
  */
 package ie4100r;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  *
  * @author Kevin-Notebook
  */
+//d.csv -> 139, BF blank
 public class Main {
+
     public static void main(String[] args) {
-        
+        int[] a = readCsvFile1Dim("data/a.csv");
+        int[][] l = readCsvFile2Dim("data/l.csv");
+        for (int v : a) {
+            System.out.print(v + " ");
+        }
+        System.out.println("\n\n");
+        for (int i = 0; i < l.length; i++) {
+            for (int j = 0; j < l[i].length; j++) {
+                System.out.print(String.format("%1$-7s", l[i][j]));
+            }
+            System.out.println();
+        }
     }
-    
-    private static void readCsvFile(String fileName) {
-        
+
+    private static int[] readCsvFile1Dim(String fileName) {
+        String line = "";
+        ArrayList<Integer> values = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            while ((line = br.readLine()) != null) {
+                // csv is a row
+                values.add(Integer.parseInt(line));
+            }
+            
+            return values.stream().mapToInt(v -> v).toArray();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private static int[][] readCsvFile2Dim(String fileName) {
+        String line = "";
+        String cvsSplitBy = ",";
+        ArrayList<int[]> values = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            while ((line = br.readLine()) != null) {
+                // use comma as separator
+                int[] entries = Arrays.stream(line.split(cvsSplitBy)).mapToInt(Integer::parseInt).toArray();
+                values.add(entries);
+            }
+            
+            return values.stream().map(v -> v).toArray(int[][]::new);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
