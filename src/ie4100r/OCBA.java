8ec44828517c17a0e5fcaf2e18e21a187718ab75
DESKTOP-H13VE6F\Kevin <dragoN414@gmail.com>
2018-02-08 00:56:27 +0800

    for testing purposes

--- src/ie4100r/OCBA.java
+++ src/ie4100r/OCBA.java
@@ -6,9 +6,16 @@
 package ie4100r;
 
 import FileManager.CsvReader;
+import Logic.OCBASolver;
+import Utils.RNG;
+import ilog.concert.IloConstraint;
+import ilog.concert.IloException;
+import ilog.concert.IloIntVar;
+import ilog.concert.IloObjective;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 
@@ -17,32 +24,90 @@ import java.util.HashMap;
  * @author Kevin-Notebook
  */
 public class OCBA {
+    RNG random = new RNG();
     public static void main(String[] args){
-        int[][] y = new int[20][];
-        for (int i = 0; i < 20; i++) {
+        //fixed parameters
+        int[] a = CsvReader.readCsvFile1Dim("data/a.csv"); //demand at residential nodes
+        int[] b = CsvReader.readCsvFile1Dim("data/b.csv"); //demand at MRT nodes
+        int[][] d = CsvReader.readCsvFile2Dim("data/d.csv");
+        int[][] e = CsvReader.readCsvFile2Dim("data/e.csv");
+        int[][] h = CsvReader.readCsvFile2Dim("data/h.csv");
+        int[][] l = CsvReader.readCsvFile2Dim("data/l.csv");
+
+        int p = 100; //number of new lockers
+        int C = 540; //locker capacity
+        int S = 1250; //distance permitted
+        
+        int k = 20; //number of designs
+        
+        //variable parameters
+        double[] alpha = new double[a.length], beta = new double[b.length];
+        int[][] y = new int[k][];
+        for (int i = 0; i < k; i++) {
             y[i] = CsvReader.readCsvFileForY(String.format("output/%d/y.csv", i + 1));
         }
-        String folderName = "ocba";
+        
+        ArrayList<HashMap<String, Double>>[] results = new ArrayList[k];
+        ArrayList<Integer>[] iterations = new ArrayList[k];
+        Arrays.parallelSetAll(results, i -> new ArrayList<>());
+        Arrays.parallelSetAll(iterations, i -> new ArrayList<>());
+        
+        double[][] output = new double[20][4];
+        
+        final double demandCoeff = 1.0, distanceCoeff = -0.2, lockerCoeff = -100;
         
         try {
-            File dir = new File(folderName);
-            if (!dir.exists()) {
-                dir.mkdir();
+            OCBASolver solver = new OCBASolver(a, b, d, e, h, l, p, C, S);
+            solver.initVariablesAndOtherConstraints();
+
+            double[] J = new double[k]; //sample means
+            double[] s = new double[k]; //sample sd
+            
+            for (int i = 0; i < k; i++) {
+                System.out.println("Design " + (i + 1));
+                solver.setY(Arrays.stream(y[i]).sum());
+                IloConstraint[] binaryConstraints = solver.addBinaryConstraints(y[i]);
+                
+                Arrays.fill(alpha, 0.0317725);
+                Arrays.fill(beta, 0.01588);
+                
+                IloConstraint[] demandConstraints = solver.addDemandConstraints(alpha, beta);
+
+                IloObjective objective = solver.defineObjectives(demandCoeff, distanceCoeff, lockerCoeff);
+                HashMap<String, Double> result = solver.solve();
+                if (result != null) {
+                    output[i][0] = result.get("total");
+                    output[i][1] = result.get("demand");
+                    output[i][2] = result.get("distance");
+                    output[i][3] = result.get("locker");
+                }
+
+                solver.deleteObjective(objective);
+                solver.deleteConstraint(demandConstraints);
+                solver.deleteConstraint(binaryConstraints);
             }
             
-            FileWriter writer3 = new FileWriter(String.format("%s\\locker.csv", folderName));
-            
-            for (int i = 0; i < y.length; i++) {
-                for (int j = 0; j < 10; j++) {
-                    writer3.append(Integer.toString(Arrays.stream(y[i]).sum()));
-                    writer3.append(',');
+            writeToCsv2Dim(output);
+        } catch (IloException ex) {
+            ex.printStackTrace();
+        }
+    }
+    
+    public static void writeToCsv2Dim(double[][] output) {
+        try {
+            FileWriter writer = new FileWriter("stuff.csv");
+
+            for (int i1 = 0; i1 < output.length; i1++) {
+                for (int i2 = 0; i2 < output[i1].length; i2++) {
+                    writer.append(Double.toString(output[i1][i2]));
+                    writer.append(',');
                 }
-                writer3.append('\n');
+                writer.append('\n');
             }
 
             //generate whatever data you want
-            writer3.flush();
-            writer3.close();
+            writer.flush();
+            writer.close();
         } catch (IOException e) {
             System.err.println("Error writing to file " + e.getCause().toString());
         }
